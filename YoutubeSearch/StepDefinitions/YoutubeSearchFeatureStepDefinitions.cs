c222261929c9035ac5f0553feadecc3ca93c04b9
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2023-02-04 16:17:36 -0500

    Scenerio Context and Dependency Injection Example

--- YoutubeSearch/StepDefinitions/YoutubeSearchFeatureStepDefinitions.cs
+++ YoutubeSearch/StepDefinitions/YoutubeSearchFeatureStepDefinitions.cs
@@ -39,14 +39,15 @@ namespace SepcflowSelenium.StepDefinitions
         [Given(@"I have entered (.*) as search keyword")]
         public void GivenIHaveEnteredBangladeshAsSearchKeyword(String searchString)
         {
-            this.searchKeyword = searchString.ToLower();
+            ScenarioContext.Current["searchKey"] = searchString.ToLower();
+            searchString= searchString.ToLower();
             // assigns search string from scenerio to the search keyword variable
             var searchInputBox = _webdriverContext.driver.FindElement(By.XPath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"));
             //find search box with full xpath and assign it to new variable
             var wait = new WebDriverWait(_webdriverContext.driver, TimeSpan.FromSeconds(2));
             wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]")));
             //wait 2 seconds for the search box to render and see if search box is visible
-            searchInputBox.SendKeys(searchKeyword);
+            searchInputBox.SendKeys(searchString);
             //send search keyword string to search inputbox
         }
 
@@ -63,8 +64,9 @@ namespace SepcflowSelenium.StepDefinitions
     public void ThenIShouldNavigateToSearchResultsPage()
     {
 
-        System.Threading.Thread.Sleep(5000); //pause thread for 5 seconds
-        // After search is complete the keyword should be present in url as well as page title`
+        System.Threading.Thread.Sleep(2000); //pause thread for 5 seconds
+            var expectedSearchKey = (String)ScenarioContext.Current["searchKey"];
+            // After search is complete the keyword should be present in url as well as page title`
         Assert.IsTrue(_webdriverContext.driver.Url.ToLower().Contains(searchKeyword));
         Assert.IsTrue(_webdriverContext.driver.Title.ToLower().Contains(searchKeyword));
 
