a03dfb8815309dfe1710f5b9a54c1dbd9540bb12
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2023-02-04 14:27:30 -0500

    Scenerio Context example

---
+++ YoutubeSearch/Context/WebdriverContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace YoutubeSearch.Context
+{
+    public class WebdriverContext
+    {
+        public ChromeDriver driver; //decalre chrome driver object
+
+        public WebdriverContext()  //decalare a constructor to initalize this object
+        {
+            driver = new ChromeDriver(); //POCO object for webdriver
+        } 
+    }
+}
--- YoutubeSearch/StepDefinitions/YoutubeSearchFeatureStepDefinitions.cs
+++ YoutubeSearch/StepDefinitions/YoutubeSearchFeatureStepDefinitions.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using TechTalk.SpecFlow;
+using YoutubeSearch.Context;
 
 namespace SepcflowSelenium.StepDefinitions
 {
@@ -15,18 +16,23 @@ namespace SepcflowSelenium.StepDefinitions
     {
         private String searchKeyword;
 
-        private ChromeDriver chromeDriver;
+        private readonly WebdriverContext _webdriverContext; //create a object
 
-        public YoutubeSearchFeatureSteps() => chromeDriver = new ChromeDriver();
+        public YoutubeSearchFeatureSteps(WebdriverContext webdriverContext)
+        {
+            this._webdriverContext = webdriverContext;
+        }
 
         //Instantiate the chrome driver
 
         [Given(@"I have navigated to youtube website")]
+
+
         public void GivenIHaveNavigatedToYoutubeWebsite()
         {
-            chromeDriver.Navigate().GoToUrl("https://www.youtube.com");
+            _webdriverContext.driver.Navigate().GoToUrl("https://www.youtube.com");
             //go to youtube using the URL
-            Assert.IsTrue(chromeDriver.Title.ToLower().Contains("youtube"));
+            Assert.IsTrue(_webdriverContext.driver.Title.ToLower().Contains("youtube"));
             //verify if you are at youtube by checking the title to see if it contains youtube        
         }
 
@@ -35,9 +41,9 @@ namespace SepcflowSelenium.StepDefinitions
         {
             this.searchKeyword = searchString.ToLower();
             // assigns search string from scenerio to the search keyword variable
-            var searchInputBox = chromeDriver.FindElement(By.XPath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"));
+            var searchInputBox = _webdriverContext.driver.FindElement(By.XPath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"));
             //find search box with full xpath and assign it to new variable
-            var wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(2));
+            var wait = new WebDriverWait(_webdriverContext.driver, TimeSpan.FromSeconds(2));
             wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]")));
             //wait 2 seconds for the search box to render and see if search box is visible
             searchInputBox.SendKeys(searchKeyword);
@@ -47,7 +53,7 @@ namespace SepcflowSelenium.StepDefinitions
         [When(@"I press the search button")]
                 public void WhenIPressTheSearchButton()
 {
-                var searchButton = chromeDriver.FindElement(By.CssSelector("button#search-icon-legacy"));
+                var searchButton = _webdriverContext.driver.FindElement(By.CssSelector("button#search-icon-legacy"));
                    //find the search button using the css selector. Xpath can also be used
                     searchButton.Click();
                     //click on the search button
@@ -59,8 +65,8 @@ namespace SepcflowSelenium.StepDefinitions
 
         System.Threading.Thread.Sleep(5000); //pause thread for 5 seconds
         // After search is complete the keyword should be present in url as well as page title`
-        Assert.IsTrue(chromeDriver.Url.ToLower().Contains(searchKeyword));
-        Assert.IsTrue(chromeDriver.Title.ToLower().Contains(searchKeyword));
+        Assert.IsTrue(_webdriverContext.driver.Url.ToLower().Contains(searchKeyword));
+        Assert.IsTrue(_webdriverContext.driver.Title.ToLower().Contains(searchKeyword));
 
         
         }
@@ -68,10 +74,10 @@ namespace SepcflowSelenium.StepDefinitions
 
     public void Dispose()
     {
-          if (chromeDriver != null)
+          if (_webdriverContext.driver != null)
         {
-            chromeDriver.Dispose();
-            chromeDriver = null;
+            _webdriverContext.driver.Dispose();
+            _webdriverContext.driver = null;
         }
     }
 }
